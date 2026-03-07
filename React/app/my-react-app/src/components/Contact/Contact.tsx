1ed787fff6329f086defcc23b7802ee92596e191
author-unknown87 <daemondog@gmail.com>
2026-03-06 22:49:18 -0500

    Added pattern assets, updated banners

--- React/app/my-react-app/src/components/Contact/Contact.tsx
+++ React/app/my-react-app/src/components/Contact/Contact.tsx
@@ -4,27 +4,30 @@ import Styles from './Contact.module.scss';
 export default function Contact() {
     return (
         <>
-            <h1>Contact Information</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed felis lectus, euismod eget mi eget, rutrum porttitor augue. Nullam dignissim mi ut diam mollis, a tempor urna porta. Integer vestibulum vel sem id iaculis. Aenean vulputate libero ut bibendum imperdiet. Sed sit amet egestas nibh, ac consectetur nibh.</p>
-            <div className={Styles.ContactCardGrid}>
-                <ContactCard 
-                    className={Styles.ContactCard}
-                    title="Phone Number"
-                    description="consectetur adipiscing elit. Sed felis lectus"
-                    content="513-515-0842"
-                />
-                <ContactCard 
-                    className={Styles.ContactCard}
-                    title="Email"
-                    description="consectetur adipiscing elit. Sed felis lectus"
-                    content="j.gravatt87@gmail.com"
-                />
-                <ContactCard 
-                    className={Styles.ContactCard}
-                    title="LinkedIn"
-                    description="consectetur adipiscing elit. Sed felis lectus"
-                    content="www.linkedin.com"
-                />
+            <div className={Styles.BannerImage}></div>
+            <div className={Styles.Main}>
+                <h1>Contact Information</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed felis lectus, euismod eget mi eget, rutrum porttitor augue. Nullam dignissim mi ut diam mollis, a tempor urna porta. Integer vestibulum vel sem id iaculis. Aenean vulputate libero ut bibendum imperdiet. Sed sit amet egestas nibh, ac consectetur nibh.</p>
+                <div className={Styles.ContactCardGrid}>
+                    <ContactCard 
+                        className={Styles.ContactCard}
+                        title="Phone Number"
+                        description="consectetur adipiscing elit. Sed felis lectus"
+                        content="513-515-0842"
+                    />
+                    <ContactCard 
+                        className={Styles.ContactCard}
+                        title="Email"
+                        description="consectetur adipiscing elit. Sed felis lectus"
+                        content="j.gravatt87@gmail.com"
+                    />
+                    <ContactCard 
+                        className={Styles.ContactCard}
+                        title="LinkedIn"
+                        description="consectetur adipiscing elit. Sed felis lectus"
+                        content="www.linkedin.com"
+                    />
+                </div>
             </div>
         </>
     )
--- React/app/my-react-app/src/components/Home/Home.tsx
+++ React/app/my-react-app/src/components/Home/Home.tsx
@@ -1,10 +1,9 @@
-import banner from '../../assets/HomeBanner.jpg';
 import styles from './Home.module.scss';
 
 export default function HomePage() {
     return (
         <>
-            <div className={styles.BannerImage}><img src={banner} /></div>
+            <div className={styles.BannerImage}></div>
             <div className={styles.Content}>
                 <h1>My name is Joshua Gravatt</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec blandit erat metus, vitae tincidunt lacus rhoncus ac. Donec sit amet commodo urna. Phasellus tincidunt pellentesque turpis nec gravida. Suspendisse vitae orci tortor. Ut eu molestie quam. Donec laoreet leo auctor consectetur molestie. Donec efficitur, augue sit amet suscipit placerat, nulla turpis consequat odio, et interdum tellus leo in neque. Proin sed tempor ante. In orci massa, pharetra vel pharetra nec, suscipit a neque. Etiam justo sem, vulputate finibus est eu, tempor lacinia ex. Suspendisse in tellus enim. Ut aliquet metus eros, hendrerit consectetur felis posuere eget. Fusce scelerisque scelerisque quam sagittis tristique.
@@ -13,4 +12,4 @@ Quisque placerat erat eget risus pretium, sit amet maximus nulla facilisis. Null
             </div>
         </>
     )
-}
+}       
