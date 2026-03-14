da35e40d828ec712ba6d4755266a8a7e178946d1
author-unknown87 <daemondog@gmail.com>
2026-03-14 13:50:26 -0400

    added github link and updated sidebar

--- React/app/my-react-app/src/components/Sidebar/app-sidebar.tsx
+++ React/app/my-react-app/src/components/Sidebar/app-sidebar.tsx
@@ -2,6 +2,7 @@ import { Pages } from "../../models/enums";
 import { SidebarItems } from "../../models/SidebarItems"
 import { Home, BookType, Headset, Folder} from "lucide-react"
 import styles from './app-sidebar.module.scss'
+import { MarkGithubIcon } from "@primer/octicons-react";
 
 import {
     Sidebar,
@@ -38,6 +39,12 @@ const items = [
         url: "#",
         icon: Folder,
         page: Pages.Projects
+    },
+    {
+        title: "Github",
+        url: "https://github.com/author-unknown87/JayDash",
+        icon: Folder,
+        page: ""
     }
 ] satisfies readonly SidebarItems[]
 
@@ -55,20 +62,27 @@ export function AppSidebar({
                     <SidebarGroupLabel>Joshua Gravatt's Site</SidebarGroupLabel>
                     <SidebarGroupContent>
                         <SidebarMenu>
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.title}
-                                    className="ml-2 my-3"
-                                >
-                                    <SidebarMenuButton className="h-12" onClick={() => {
-                                        onClick(item.page)
-                                    }}>
-                                        <a href={item.url} className="w-full">
-                                            <item.icon />
-                                            <span>{item.title}</span>
-                                        </a>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
+                            {items.map((item) => {
+                                const isGithubLink = item.title === "Github";
+
+                                const Icon = isGithubLink ? MarkGithubIcon : item.icon;
+                                return (
+                                    <SidebarMenuItem key={item.title}
+                                        className="ml-2 my-3"
+                                    >
+                                        <SidebarMenuButton className="h-12" onClick={() => {
+                                            if (!isGithubLink) {
+                                                onClick(item.page)
+                                            }
+                                        }}>
+                                            <a href={item.url} target={isGithubLink ? "_blank" : ""} className="w-full">
+                                                <Icon className={styles.SidebarIcon}/>         
+                                                <span>{item.title}</span>
+                                            </a>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                    )
+                            })}
                         </SidebarMenu>
                     </SidebarGroupContent>
                 </SidebarGroup>
