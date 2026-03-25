71b37905e38c7fbbac88f70dde46df143b6786a9
author-unknown87 <daemondog@gmail.com>
2026-03-24 23:16:58 -0400

    Serilog installed

--- Backend/Controllers/ResumeController.cs
+++ Backend/Controllers/ResumeController.cs
@@ -6,11 +6,15 @@ namespace JayDash.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ResumeController(AppDbContext _context, IResumeService _resumeService) : ControllerBase
+public class ResumeController(
+    AppDbContext _context, 
+    IResumeService _resumeService,
+    ILogger<ResumeController> _logger) : ControllerBase
 {
     [HttpGet]
     public async Task<IActionResult> GetResume(CancellationToken cancellationToken = default)
     {
+        _logger.LogInformation("Resume controller called.  Endpoint: GetResume");
         var resume = await _resumeService.GetResume(cancellationToken);
         return Ok(resume);
     }
