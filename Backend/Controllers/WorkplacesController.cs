c7057746489d2a4b2ebf23b274852de37e260466
author-unknown87 <daemondog@gmail.com>
2026-03-29 13:05:47 -0400

    Small update to workplace repo and controller

--- Backend/Controllers/WorkplacesController.cs
+++ Backend/Controllers/WorkplacesController.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using JayDash.Data.Models;
 using JayDash.Repositories.Interfaces;
+using JayDash.Repositories.Specifications;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JayDash.Controllers;
@@ -12,17 +13,26 @@ namespace JayDash.Controllers;
 public class WorkplacesController(IWorkplaceRepository repository) : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<WorkplaceModel>>> Get(CancellationToken cancellationToken = default)
+    public async Task<ActionResult<List<WorkplaceModel>>> GetAllWorkplaces(CancellationToken cancellationToken = default)
     {
         var workplaces = await repository.GetAllWorkplaces(cancellationToken: cancellationToken);
         return Ok(workplaces);
+    }
+
+    [HttpGet("by-name")]
+    public async Task<ActionResult<WorkplaceModel>> GetWorkplaceByCompanyName([FromQuery] string companyName, CancellationToken cancellationToken = default)
+    {
+        // TODO: need to validate data
+        var companyNameSpec = new GetWorkplaceByCompanyNameSpec(companyName);
+        var workplace = await repository.GetAllWorkplaces(cancellationToken, companyNameSpec);
+        return Ok(workplace);
     }
 
     [HttpPost]
     public async Task<IActionResult> Post([FromBody] WorkplaceModel model, CancellationToken cancellationToken = default)
     {
         // Stub: implement create logic using repository when available.
-        return CreatedAtAction(nameof(Get), null, model);
+        return CreatedAtAction("test", null, model);
     }
 
     [HttpDelete("{id:int}")]
